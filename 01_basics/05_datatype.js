9befe8693cf69664c92cf1acc82503eb60cf41d6
devnaresh0 <nareshkumar0000002@gmail.com>
2024-10-23 05:11:06 +0000

    data type summary

---
+++ 01_basics/05_datatype.js
@@ -0,0 +1,13 @@
+// Primitive
+
+// 7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+const score = 100;
+// Non-primitve
+
+// Reference type {Non Primitive}
+
+// Array, Objects,Functions
+
+
+
+// the data type of non primitive data type is function
