d9f81bedbf8a56c53d61371a65956401fd9c1036
devnaresh0 <nareshkumar0000002@gmail.com>
2024-10-24 15:58:19 +0000

    9:34

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+// Dates
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 0,23,6,3);
+console.log(myCreatedDate.toDateString());
+// 9 : 34
