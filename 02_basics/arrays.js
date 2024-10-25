9047a08afc7380baed65d9535908741b45f132d1
devnaresh0 <nareshkumar0000002@gmail.com>
2024-10-25 03:18:26 +0000

    array_basic

---
+++ 02_basics/arrays.js
@@ -0,0 +1,24 @@
+// // array
+
+const myArr = [0,1,2,3,4,5] // elements
+// const myHeros = ["Shaktiman","naagaraj"]
+// console.log(myArr[4]);
+// const myArr2 = new Array(1,2,3,4,5);
+// myArr2.push(0)
+// myArr2.unshift(9)
+// console.log(myArr2);
+// myArr.pop(5)
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// slice, splice
+ console.log("A ",myArr);
+ const myn1 = myArr.slice(1,3);
+ console.log(myn1);
+ console.log("B ",myArr)
+ const myn2 = myArr.splice(1,3);
+ console.log("C ",myArr);
+ 
+console.log(myn2);
