04379b85e81d1ad5b7493bcf4990e574813f2424
devnaresh0 <nareshkumar0000002@gmail.com>
2024-10-25 02:44:05 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -8,3 +8,12 @@ console.log(typeof myDate);
 let myCreatedDate = new Date(2023, 0,23,6,3);
 console.log(myCreatedDate.toDateString());
 // 9 : 34
+let myTimeStamp = Date.now();
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getFullYear());
+newDate.toLocaleString('default',{
+    weekday: "long",
+    timeZone: '',
+})
