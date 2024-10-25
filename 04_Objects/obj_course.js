299e203a53c59ff9659ae967f902ef3ab19995aa
devnaresh0 <nareshkumar0000002@gmail.com>
2024-10-25 09:39:04 +0000

    9:00

---
+++ 04_Objects/obj_course.js
@@ -0,0 +1,18 @@
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh",
+}
+const {courseInstructor: instructor} = course
+console.log(instructor);
+const navbar = ({company}) => {
+
+}
+
+navbar(company = "hitesh")
+// {
+//     name: "hitesh",
+//     coursename: "js in hindi",
+//     price: "free"
+// }
+// 9:00
