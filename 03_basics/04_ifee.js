7df8bc9d77e9589ddc041c6e53bdf62ebce2dc4b
devnaresh0 <nareshkumar0000002@gmail.com>
2024-10-27 16:06:28 +0000

    ifee

--- 03_basics/04_ifee.js
+++ 03_basics/04_ifee.js
@@ -1,10 +1,20 @@
 // Immedialty Invoked Function Expression (IIFE)
 (
 function chai() {
+    // named ifee
     console.log('DB connected');
     
-})()
+})();
+
+(
+    // unnamed ifee
+    (name) => {
+        console.log(`DD Connedcted ${name}`);
+        
+    })('hitesh')
+
 
 // 4:00 Video NO. 24
 
 // chai();
+
