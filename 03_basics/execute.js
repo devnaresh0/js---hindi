45d4126ad1462dc28a8ff8a26e7bf8e0b12709be
devnaresh0 <nareshkumar0000002@gmail.com>
2024-10-28 01:48:22 +0000

    execute

---
+++ 03_basics/execute.js
@@ -0,0 +1,22 @@
+// Function Execution phase
+// Eval Execution phasde
+
+// {} => Memmory execution phase
+// execution phase
+// 1. Global Execution
+// 2. Memory Phase
+// val 1 -> undefined
+// val 2 => undefined
+// addnum => defination
+// result1 => undefined
+// result2 => undefined
+// 3. Execution phase
+// val 1 = 10
+// val 2 = 5
+// addNum => [ new varaiable enveiournment + execution thread]
+// everytime we write function Memory phase and execution pnase executes
+// go inside function not directly
+// deletion after work
+// NVE + thread
+// call stack
+// Last in first Out
